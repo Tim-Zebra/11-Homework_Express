5c9972053b7c703ed51d21f14add12fa7e4a47db
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-04-29 18:11:30 -0500

    Added partial delete function.

--- routes/notes.js
+++ routes/notes.js
@@ -1,13 +1,14 @@
 const notes = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
+const notesArr = require('.db/db');
 
-// GET Route for retrieving all the tips
+// GET Route for retrieving stored notes
 notes.get('/api/notes', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// POST Route for a new UX/UI tip
+// POST Route for a new note
 notes.post('/api/notes', (req, res) => {
   const { title, text } = req.body;
 
@@ -15,7 +16,7 @@ notes.post('/api/notes', (req, res) => {
     const newNote = {
         title,
         text,
-        note_id: uuid(),
+        id: uuid(),
     };
 
     readAndAppend(newNote, './db/db.json');
@@ -24,5 +25,23 @@ notes.post('/api/notes', (req, res) => {
     res.error('Error in adding note ❌');
   }
 });
+
+// DELETE Route to delete a note based off id
+notes.delete('/api/notes/:id', (req, res) => {
+    if (req.params.id) {
+      console.info(`${req.method} request received to delete a note`);
+      const noteId = req.params.id;
+      for (let i = 0; i < notesArr.length; i++) {
+        const currentNote = notesArr[i];
+        if (currentNote.id === reviewId) {
+          res.json(currentNote);
+          return;
+        }
+      }
+      res.status(404).send('Note not found');
+    } else {
+      res.status(400).send('Note ID not provided');
+    }
+  });
 
 module.exports = notes;
